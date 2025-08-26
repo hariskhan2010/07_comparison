febeb07dd7a16e64ae04cdec252ff2d3d2aa06cb
hariskhan2010 <harismalik2010afridi@gmail.com>
2025-08-26 11:09:45 +0500

    Add files via upload

---
+++ 07_comparison.py
@@ -0,0 +1,56 @@
+#comparison oprators
+# ==,!=, <, >
+
+# password = "1234"
+# user_password = input("Enter Your Password: ")
+
+# if password == user_password:
+#     print("Correct Password")
+# else:
+#     print("Invalid Password")
+
+# name1 = "Haris"
+# name2 = "haris"
+# name_check= name1 == name2
+# print(name_check)
+
+# number1 = 40
+# number2 = 48
+# name_checker = number1 == number2
+# print(name_checker)
+# if number1 == number2:
+#     print("both are equal")
+# else:
+#     print ("not equal")
+
+# honda = 400
+# toyota = 600
+# suzuki = 500 
+# bmw = 400
+# mercedes = 600
+# lixus = 500
+# all_car1 = honda, suzuki,lixus
+# all_car2 = bmw, mercedes,toyota
+# number_checker = all_car1 == all_car2
+# print(number_checker)
+# if all_car1 == all_car2:
+#     print("both {all_cars}are same")
+# else:
+#     print(f"not {all_car1} same")
+
+
+
+
+
+
+
+
+# number3 = 400
+# number4 = 300
+# name_checker = number3 == number4
+# print(name_checker)
+# if number3 == number4:
+#     print("both are same")
+# else:
+#     print("not same")
+
